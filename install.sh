cede6fe7cbfcc42dc8c00656a6d4583c55cfdeda
miguel <miguel@thedeanda.com>
2014-04-30 23:03:59 -0700

    updated settings, install to local repo script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mvn install:install-file \
+	-Dfile=dist/mongo-object-map-2014-04-20.jar \
+	-DgroupId=com.thedeanda \
+	-DartifactId=mongo-object-map \
+	-Dversion=2014-04-20 \
+	-Dpackaging=jar
+
+
